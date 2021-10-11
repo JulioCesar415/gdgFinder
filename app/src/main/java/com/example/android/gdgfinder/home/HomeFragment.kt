868f8596e368fd37df923afbfb6a39182b434f0f
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-10-10 19:05:40 -0700

    region.xml

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
@@ -28,6 +30,15 @@ class HomeFragment : Fragment() {
         val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         binding.viewModel = viewModel
+
+//        register observer on navigateToSearch
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer<Boolean>{ shouldNavigate ->
+            if (shouldNavigate == true){
+                val navController = binding.root.findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
 
         return view
     }
